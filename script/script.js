b7cdd4210c405c714a6b238dd0dfac73b8844506
lagunatlt <lagunatlt@yandex.ru>
2019-12-18 05:19:14 +0300

    ок

--- script/script.js
+++ script/script.js
@@ -2,8 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const customer = document.getElementById('customer');
     const freelancer = document.getElementById('freelancer');
+    const blockCustomer = document.querySelector('#block-customer');
+    const blockFreelancer = document.querySelector('#block-freelancer');
 
-    customer.addEventListener('contextmenu', () => {
-        console.log('что-то произошло')
-    })
+
+    customer.addEventListener('click', () => {
+        blockCustomer.style.display = 'block';
+    });
+
+    freelancer.addEventListener('click', () => {
+        blockFreelancer.style.display = 'block';
+    });
 });
