c5ff0184ad0de194f732b6ae07281f5162bc2aab
lagunatlt <lagunatlt@yandex.ru>
2019-12-22 15:12:38 +0400

    ок

--- script/script.js
+++ script/script.js
@@ -1,16 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     
-    const customer = document.getElementById('customer');
-    const freelancer = document.getElementById('freelancer');
-    const blockCustomer = document.querySelector('#block-customer');
-    const blockFreelancer = document.querySelector('#block-freelancer');
+    const customer = document.getElementById('customer'),
+        freelancer = document.getElementById('freelancer'),
+        blockCustomer = document.getElementById('block-customer'),
+        blockFreelancer = document.getElementById('block-freelancer'),
+        blockChoice = document.getElementById('block-choice'),
+        btnExit = document.getElementById('btn-exit'),
+        formCustomer = document.getElementById('form-customer');
 
 
     customer.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
         blockCustomer.style.display = 'block';
+        btnExit.style.display = 'block';
     });
 
     freelancer.addEventListener('click', () => {
+        blockChoice.style.display = 'none';
         blockFreelancer.style.display = 'block';
+        btnExit.style.display = 'block';
     });
+
+    btnExit.addEventListener('click', () => {
+        btnExit.style.display = 'none';
+        blockCustomer.style.display = 'none';
+        blockFreelancer.style.display = 'none';
+        blockChoice.style.display = 'block';
+    })
 });
